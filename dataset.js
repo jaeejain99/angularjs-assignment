4ac4268f05737893e9f9fc72eea3f01127301fdf
jaeejain99 <56268462+jaeejain99@users.noreply.github.com>
2019-10-07 22:39:36 +0530

    Add files via upload

---
+++ dataset.js
@@ -0,0 +1,45 @@
+var x=angular.module('searchName',[]);
+x.controller("nameController",function($scope)
+{
+$scope.namedata=[
+
+{name:'Operating systems'},
+{name:'System programming'},
+{name:'Structures'},
+{name:'Algorithm'},
+{name:'Java'},
+{name:'PHP'},
+{name:'JavaScript'},
+{name:'Angularjs'},
+{name:'Android'},
+{name:'Swing'},
+{name:'Machine learning'},
+{name:'Linux'},
+{name:'Flutter'},
+{name:'CSS'},
+{name:'Autolisp'},
+{name:'Animation'},
+{name:'Dataset'},
+{name:'Opencv'},
+{name:'Linked list'},
+{name:'Design pattern'},
+{name:'Sorting'},
+{name:'Google'},
+{name:'Interface'},
+{name:'Assignment'},
+{name:'Technology'},
+{name:'Computer networks'},
+{name:'Artificial intelligence'},
+{name:'Matlab'},
+{name:'Ruby'},
+{name:'Program'},
+{name:'Source code'},
+{name:'Hypertext language'},
+{name:'Sublime text'},
+{name:'Blockchain'},
+{name:'Hacking'},
+{name:'Internet of Things'},
+{name:'Webdevelopment'}
+]
+}
+);
